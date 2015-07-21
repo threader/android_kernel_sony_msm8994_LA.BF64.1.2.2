cf3416d9c3bb7f433346a85add1a1b8cceab80f7
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2015-07-21 10:30:01 -0700

    sched: Avoid running idle_balance() consecutively
    With the introduction of "e4600ab9eb sched: update ld_moved for active
    balance from the load balancer" the function load_balance() returns a
    non zero number of migrated tasks in anticipation of tasks that will
    end up on that CPU via active migration. Unfortunately on kernel
    versions 3.14 and beyond this ends up breaking pick_next_task_fair()
    which assumes that the load balancer only returns non zero numbers for
    tasks already migrated on to the destination CPU. A non zero number
    then triggers a rerun of the pick_next_task_fair() logic so that it
    can return one of the migrated tasks as the next task. When the load
    balancer returns a non zero number for tasks that will be moved via
    active migration, the rerun of pick_next_task_fair() finds the CPU to
    still have no runnable tasks. This in turn causes a rerun of
    idle_balance() and possibly migrating another task. Hence the
    destination CPU can unintentionally end up pulling several tasks.
    
    The intent of the change above is still necessary though to indicate
    termination of load balance at higher scheduling domains when active
    migration occurs. Achieve the same effect by using continue_balancing
    instead of faking the number of pulled tasks. This way
    pick_next_task_fair() stays happy and load balance stops at higher
    scheduling domains.
    
    Change-Id: Id223a3287e5d401e10fbc67316f8551303c7ff96
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [@mortezadadgar: backport to 3.10]
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>
    (cherry picked from commit 18a11495e0e1b908cdd8ac668f8ac40e07806352)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7672,7 +7672,7 @@ no_move:
 				stop_one_cpu_nowait(cpu_of(busiest),
 					active_load_balance_cpu_stop, busiest,
 					&busiest->active_balance_work);
-				ld_moved++;
+				*balance = 0;
 			}
 
 			/*
@@ -7811,9 +7811,12 @@ void idle_balance(struct rq *this_rq)
 
 		/*
 		 * Stop searching for tasks to pull if there are
-		 * now runnable tasks on this rq.
+		 * now runnable tasks on this rq or if
+		 * balance has been unset (only possible
+		 * due to active migration).
 		 */
-		if (pulled_task || this_rq->nr_running > 0) {
+		if (pulled_task || this_rq->nr_running > 0 ||
+						!balance) {
 			balance_rq->idle_stamp = 0;
 			break;
 		}

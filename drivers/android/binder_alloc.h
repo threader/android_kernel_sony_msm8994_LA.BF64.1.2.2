1bb15eab4b30404c5d1092a42d923eb1cbfc5476
Todd Kjos <tkjos@android.com>
2019-02-08 10:35:14 -0800

    UPSTREAM: binder: create userspace-to-binder-buffer copy function
    The binder driver uses a vm_area to map the per-process
    binder buffer space. For 32-bit android devices, this is
    now taking too much vmalloc space. This patch removes
    the use of vm_area when copying the transaction data
    from the sender to the buffer space. Instead of using
    copy_from_user() for multi-page copies, it now uses
    binder_alloc_copy_user_to_buffer() which uses kmap()
    and kunmap() to map each page, and uses copy_from_user()
    for copying to that page.
    
    (cherry picked from 1a7c3d9bb7a926e88d5f57643e75ad1abfc55013)
    Bug: 67668716
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Change-Id: I59ff83455984fce4626476e30601ed8b99858a92

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -22,6 +22,7 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/list_lru.h>
+#include <uapi/linux/android/binder.h>
 
 extern struct list_lru binder_alloc_lru;
 struct binder_transaction;
